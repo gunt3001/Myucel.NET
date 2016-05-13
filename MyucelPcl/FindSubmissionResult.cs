aaebe3c2257017bed3a2a147ab791be9824b626e
Napat R <napatr@magic-code.com>
2016-05-13 11:51:15 +0700

    Myucel ported to PCL!

---
+++ MyucelPcl/FindSubmissionResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyucelPcl
+{
+    public class FindSubmissionResult
+    {
+        public string Title { get; set; }
+        public Uri Link { get; set; }
+        public float Certainty { get; set; }
+    }
+}
